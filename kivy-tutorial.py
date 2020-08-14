e9cbcb86e50aa66364cd658ab3274d1b1403d6f8
Mike-Kimani <mikalkimmani@gmail.com>
2020-08-14 07:30:09 +0300

    Bound submit button to the pressed function

--- kivy-tutorial.py
+++ kivy-tutorial.py
@@ -3,23 +3,36 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols =2
+        self.cols =1
+        self.inside = GridLayout()
+        self.inside.cols = 2
 
-        self.add_widget(Label(text = "First Name:"))
+        self.add_widget(self.inside)
+        self.inside.add_widget(Label(text = "First Name:"))
         self.first_name = TextInput(multiline = False)
-        self.add_widget(self.first_name)
+        self.inside.add_widget(self.first_name)
 
-        self.add_widget(Label(text = "Last Name:"))
+        self.inside.add_widget(Label(text = "Last Name:"))
         self.last_name = TextInput(multiline = False)
-        self.add_widget(self.last_name)
+        self.inside.add_widget(self.last_name)
 
-        self.add_widget(Label(text = "Email:"))
+        self.inside.add_widget(Label(text = "Email:"))
         self.email = TextInput(multiline = False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
+        
+        self.submit = Button(text = "Submit", font_size =40)
+        
+        self.submit.bind(on_press=self.pressed)
+        
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        print("Pressed")    
 
 class MyApp(App):
     def  build(self):
