ad5fb33abc72c21430bdb6cf32761dfbe5d8eed5
Mike-Kimani <mikalkimmani@gmail.com>
2020-08-22 17:16:45 +0300

    Using float layout

--- main.py
+++ main.py
@@ -5,14 +5,26 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MyGrid(Widget):
-    pass
+    first_name = ObjectProperty(None)
+    last_name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btn(self):
+        print("Name: " +self.first_name.text+ " "+ self.last_name.text)
+        print("Email: " + self.email.text)
+        
+        #clears the input boxes after submitting
+        self.first_name.text = " "
+        self.last_name.text = " "
+        self.email.text = " "
         
   
-class MyApp(App):
+class MeApp(App):
     def  build(self):
         return MyGrid()
 
 if __name__ == "__main__":
-    MyApp().run()
+    MeApp().run()
