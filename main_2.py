145b3ab3036665e1498725ad17f1f3abe8ebd4d5
Mike-Kimani <mikalkimmani@gmail.com>
2020-08-24 17:21:25 +0300

    Working on touch and mouse input

--- main_2.py
+++ main_2.py
@@ -1,6 +1,8 @@
 import kivy
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color
+from kivy.uix.widget import Widget
 
 class MyApp(App):
     def build(self):
